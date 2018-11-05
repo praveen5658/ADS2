349bafc0a779e28f170e68136975f5d3eae0ccdd
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-11-05 20:39:48 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m7/1. MST/Solution.java
+++ ADS - 2 Assignments/m7/1. MST/Solution.java
@@ -1,22 +1,36 @@
+/**
+ * Class.
+ */
 import java.util.Scanner;
-class Solution {
-	private Solution() {
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		String[] input;
-		EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
-		for (int i = 0; i < edges; i++) {
-			input = scan.nextLine().split(" ");
-			ewg.addEdge(new Edge(Integer.parseInt(
-			                         input[0]), Integer.parseInt(
-			                         input[1]), Double.parseDouble(
-			                         input[2])));
-		}
-		KruskalMST kmst = new KruskalMST(ewg);
-		System.out.format("%.5f\n", kmst.weight());
-	}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        String[] input;
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
+        for (int i = 0; i < edges; i++) {
+            input = scan.nextLine().split(" ");
+            ewg.addEdge(new Edge(Integer.parseInt(
+                                     input[0]), Integer.parseInt(
+                                     input[1]), Double.parseDouble(
+                                     input[2])));
+        }
+        KruskalMST kmst = new KruskalMST(ewg);
+        System.out.format("%.5f\n", kmst.weight());
+    }
 }
