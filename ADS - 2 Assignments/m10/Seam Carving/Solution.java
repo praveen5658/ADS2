c18aecc1e52cd7b9d7c4b048439ef9ec251acd5b
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-11-08 15:13:38 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m10/Seam Carving/Solution.java
+++ ADS - 2 Assignments/m10/Seam Carving/Solution.java
@@ -109,17 +109,17 @@ public class Solution {
             //     }
             //     break;
 
-            // case "removeHorizontalSeam removeVerticalSeam":
-            //     while (scan.hasNextLine()) {
-            //         String file = scan.nextLine();
-            //         seamCarver = new SeamCarver(new Picture("/Files/" + file));
-            //         int[] horizontalSeam = seamCarver.findHorizontalSeam();
-            //         seamCarver.removeHorizontalSeam(horizontalSeam);
-            //         int[] verticalSeam = seamCarver.findVerticalSeam();
-            //         seamCarver.removeVerticalSeam(verticalSeam);
-            //         printEnergies("/Files/" + file);
-            //     }
-            //     break;
+            case "removeHorizontalSeam removeVerticalSeam":
+                while (scan.hasNextLine()) {
+                    String file = scan.nextLine();
+                    seamCarver = new SeamCarver(new Picture("/Files/" + file));
+                    int[] horizontalSeam = seamCarver.findHorizontalSeam();
+                    seamCarver.removeHorizontalSeam(horizontalSeam);
+                    int[] verticalSeam = seamCarver.findVerticalSeam();
+                    seamCarver.removeVerticalSeam(verticalSeam);
+                    printEnergies("/Files/" + file);
+                }
+                break;
 
             default:
                 // seamCarver = new SeamCarver(null);
