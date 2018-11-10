669ee31db31cd4813906468c93c016670a858962
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-11-10 12:37:21 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 2 Assignments/m12/ADS-2_Exam-2/Stack.java
+++ ADS - 2 Assignments/m12/ADS-2_Exam-2/Stack.java
@@ -192,10 +192,10 @@ public class Stack<Item> implements Iterable<Item> {
         /**
          * Constructs the object.
          *
-         * @param      first  The first
+         * @param      first1  The first
          */
-        public ListIterator(final Node<Item> first) {
-            current = first;
+        public ListIterator(final Node<Item> first1) {
+            current = first1;
         }
         /**
          * Determines if it has next.
